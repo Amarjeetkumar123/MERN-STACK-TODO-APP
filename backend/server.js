a97c7946f9aa122d064fa3270c0c33212b03a4a2
Amarjeet Kumar <amarjeetkumar802212@gmail.com>
2023-04-07 16:26:09 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
-
 import { app } from "./app.js";
 import { connectDB } from "./data/database.js";
 // import { config } from "dotenv";
 connectDB;
 
 app.listen(process.env.PORT, () => {
-  console.log(`server is running at http://localhost:${process.env.PORT} in ${process.env.NODE_ENV} mode`);
+  console.log(`server is running at http://localhost:${process.env.PORT}`);
 });
